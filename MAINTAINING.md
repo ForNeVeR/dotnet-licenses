a94295c2971fd3e968d809e4b90f7b2865cbdb2c
Friedrich von Never <friedrich@fornever.me>
2024-03-29 19:20:17 +0100

    CI: add the packaging workflow

--- MAINTAINING.md
+++ MAINTAINING.md
@@ -3,9 +3,9 @@
 
 Publish a New Version
 ---------------------
-1. Update the copyright year in the `DotNetLicenses/DotNetLicenses.fsproj` file, if required.
+1. Update the copyright year in the `Directory.Build.props` file, if required.
 2. Prepare a corresponding entry in the `CHANGELOG.md` file (usually by renaming the "Unreleased" section).
-3. Set `<Version>` in the `DotNetLicenses/DotNetLicenses.fsproj` file.
+3. Set `<Version>` in the `Directory.Build.props` file.
 4. Merge the aforementioned changes via a pull request.
 5. Check if the NuGet key is still valid (see the **Rotate NuGet Publishing Key** section if it isn't).
 6. Push a tag in form of `v<VERSION>`, e.g. `v0.0.0`. GitHub Actions will do the rest (push a NuGet package).
--- scripts/github-actions.fsx
+++ scripts/github-actions.fsx
@@ -9,25 +9,71 @@ open type Generaptor.Library.Actions
 open type Generaptor.Library.Patterns
 
 let mainBranch = "main"
+let linuxImage = "ubuntu-22.04"
 let images = [
-    "macos-13"
-    "ubuntu-22.04"
+    "macos-12"
+    linuxImage
     "windows-2022"
 ]
 
 let workflows = [
-    workflow "main" [
-        name "Main"
+    let mainTriggers = [
         onPushTo mainBranch
         onPullRequestTo mainBranch
         onSchedule(day = DayOfWeek.Saturday)
         onWorkflowDispatch
+    ]
+
+    workflow "main" [
+        name "Main"
+        yield! mainTriggers
         job "main" [
             checkout
             yield! dotNetBuildAndTest()
-            step(name = "Pack", shell = "pwsh", run = "dotnet pack")
         ] |> addMatrix images
     ]
+    workflow "release" [
+        name "Release"
+        yield! mainTriggers
+        onPushTags "v*"
+        job "nuget" [
+            runsOn linuxImage
+            checkout
+            writeContentPermissions
+
+            let configuration = "Release"
+
+            let versionStepId = "version"
+            let versionField = "${{ steps." + versionStepId + ".outputs.version }}"
+            getVersionWithScript(stepId = versionStepId, scriptPath = "scripts/Get-Version.ps1")
+            dotNetPack(version = versionField)
+
+            let releaseNotes = "./release-notes.md"
+            prepareChangelog(releaseNotes)
+            let artifacts projectName includeSNuPkg = [
+                $"./{projectName}/bin/{configuration}/{projectName}.{versionField}.nupkg"
+                if includeSNuPkg then $"./{projectName}/bin/{configuration}/{projectName}.{versionField}.snupkg"
+            ]
+            let allArtifacts = [
+                yield! artifacts "DotNetLicenses" true
+            ]
+            uploadArtifacts [
+                releaseNotes
+                yield! allArtifacts
+            ]
+            yield! ifCalledOnTagPush [
+                createRelease(
+                    name = $"dotnet-licenses {versionField}",
+                    releaseNotesPath = releaseNotes,
+                    files = allArtifacts
+                )
+                yield! pushToNuGetOrg "NUGET_TOKEN" (
+                    artifacts "DotNetLicenses" false
+                )
+            ]
+        ]
+    ]
 ]
+
 
 EntryPoint.Process fsi.CommandLineArgs workflows
