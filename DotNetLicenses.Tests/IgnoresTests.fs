4b62fbff347e8c99d3a15e17ad70ad1abdf3d6e1
Friedrich von Never <friedrich@fornever.me>
2024-04-29 00:13:44 +0200

    Licenses: fix my email in more places

--- DotNetLicenses.Tests/IgnoresTests.fs
+++ DotNetLicenses.Tests/IgnoresTests.fs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 Friedrich von Never <friedrich@fornever.md>
+// SPDX-FileCopyrightText: 2024 Friedrich von Never <friedrich@fornever.me>
 //
 // SPDX-License-Identifier: MIT
 
